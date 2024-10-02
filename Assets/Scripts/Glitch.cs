715cecc2929bb93c5b985a9192e0be4035e1875a
JonasChiang0814 <chonlokc@student.unimelb.edu.au>
2024-10-02 22:29:35 +1000

    changing color for the glitch

--- Assets/Scripts/Glitch.cs
+++ Assets/Scripts/Glitch.cs
@@ -28,11 +28,14 @@ public class Glitch : MonoBehaviour
     IEnumerator StartGlitch() {
         glitchDuration = new WaitForSeconds(Random.Range(.05f,.25f));
         holoRenderer.material.SetFloat("_Amount", 1f);
-        holoRenderer.material.SetFloat("_CutOutThresh", .71f);
+        // holoRenderer.material.SetColor("_TintColor", new Color32(7, 0, 115, 255)); // blue
+        holoRenderer.material.SetColor("_TintColor", new Color32(115, 7, 0, 255)); // red
+        holoRenderer.material.SetFloat("_CutOutThresh", .3f);
         holoRenderer.material.SetFloat("_Amplitude", Random.Range(100,250));
         holoRenderer.material.SetFloat("_Speed", Random.Range(1,10));
         yield return glitchDuration;
         holoRenderer.material.SetFloat("_Amount", 0f);
         holoRenderer.material.SetFloat("_CutOutThresh", 0f);
+        holoRenderer.material.SetColor("_TintColor", new Color32(115, 101, 0, 255)); // yellow
     }
 }
